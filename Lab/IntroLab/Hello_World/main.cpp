9d13d827fff7c2f842331119dc8cc4bf6fbf289d
Daisygosorio@gmail.com <Daisygosorio@gmail.com>
2018-09-04 14:41:45 -0700

    Signed-off-by: Daisygosorio@gmail.com <Daisygosorio@gmail.com>

---
+++ Lab/IntroLab/Hello_World/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Daisy Garcia-Osorio
+ * Created on August 28, 2018, 2:28 PM
+ * Purpose: Introduction Lab Assignment
+ */
+
+#include <cstdlib>
+#include <iostream>//displays output on the screen/reads input from the keyboard
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    //Display Hello World!
+    cout<<"Hello World !"<<endl; 
+    
+    return 0;
+}
+

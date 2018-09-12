f50305003208e2cefe415e15b48210efeaaa8c87
Daisygosorio@gmail.com <Daisygosorio@gmail.com>
2018-09-12 11:46:11 -0700

    Signed-off-by: Daisygosorio@gmail.com <Daisygosorio@gmail.com>

---
+++ Book/Chapter_9_Pointers/Gaddis_8thEd_Ch.9_Program_1/main.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   main.cpp
+ * Author: Daisy Garcia-Osorio
+ * Created on September 12, 2018, 11:34 AM
+ * Purpose:  This program uses the & operator to determine a variable's address
+ *           and the sizeof operator to determine its size
+ */
+
+//System Libraries Here
+#include <iostream>// I/O LIbrary 
+#include <cmath>//Math Library
+#include <iomanip>//Formatting Library
+#include <cstdlib>//exit () function
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    int x=2;
+    
+    //Input or initialize values Here
+    cout<<"The address of x is "<<&x<<endl;
+    cout<<"The sie of x is "<<sizeof(x)<<" bytes"<<endl;
+    cout<<"The value in x is "<<x<<endl;
+    
+    
+    
+    //Process/Calculations Here
+   
+    
+    //Output Located Here
+   
+    //Exit
+    return 0;
+}

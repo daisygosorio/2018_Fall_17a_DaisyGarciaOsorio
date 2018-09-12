ae2ae5061bb4afaed0f1342af13239876e6d231d
Daisygosorio@gmail.com <Daisygosorio@gmail.com>
2018-09-12 12:31:46 -0700

    Signed-off-by: Daisygosorio@gmail.com <Daisygosorio@gmail.com>

--- Homework/Homework_1/Gaddis_9thEd_Ch.4_Prob_10_/main.cpp
+++ Homework/Homework_1/Gaddis_9thEd_Ch.4_Prob_10_/main.cpp
@@ -32,54 +32,108 @@ int main(int argc, char** argv) {
     cin>>month;
     cout<<"Enter a year"<<endl;
     cin>>year;
-    
-    //Initialize values Here
-    
-    
-    //Process/Calculations Here
-    
-    
+        
     //Identifying if inputed year is a Leap Year
     
     /*If year is divisible by 100 then it is a leap year if and only if it 
      *is divisible by 400
      */ 
-    if (year%100==0){
+    
+    if (year%100==0)
+    {
         if(year%400==0)
+        {
         cout<<"Leap year"<<endl;
-        else 
+        switch (month)
+           {
+               case 1 :cout<<"31 days "<<endl;break;
+               case 2 :cout<<"29 days "<<endl;break;
+               case 3 :cout<<"31 days "<<endl;break;
+               case 4 :cout<<"30 days "<<endl;break;
+               case 5 :cout<<"31 days "<<endl;break;
+               case 6 :cout<<"30 days "<<endl;break;
+               case 7 :cout<<"31 days "<<endl;break;
+               case 8 :cout<<"31 days "<<endl;break;
+               case 9 :cout<<"30 days "<<endl;break;
+               case 10 :cout<<"31 days "<<endl;break;
+               case 11 :cout<<"30 days "<<endl;break;
+               case 12 :cout<<"31 days "<<endl;break;
+            default:cout<<"End of Program"<<endl;break;
+
+           }
+        }
+        else
+        {
+        switch (month)
+           {
+               case 1 :cout<<"31 days "<<endl;break;
+               case 2 :cout<<"28 days "<<endl;break;
+               case 3 :cout<<"31 days "<<endl;break;
+               case 4 :cout<<"30 days "<<endl;break;
+               case 5 :cout<<"31 days "<<endl;break;
+               case 6 :cout<<"30 days "<<endl;break;
+               case 7 :cout<<"31 days "<<endl;break;
+               case 8 :cout<<"31 days "<<endl;break;
+               case 9 :cout<<"30 days "<<endl;break;
+               case 10 :cout<<"31 days "<<endl;break;
+               case 11 :cout<<"30 days "<<endl;break;
+               case 12 :cout<<"31 days "<<endl;break;
+               default:cout<<"End of Program"<<endl;break;
+
+           }
         cout<<"Not a Leap year"<<endl;
+        }
     }
     /*if year is not divisible by 100 then it is a leap year if and only if it
      *it is divisible by 4
      */
-    else{
-        if (year%4==0)
-        cout<<"Leap year"<<endl;
-        else 
-        cout<<"Not a Leap year"<<endl;
-    }
-    
-    /*Switch case tells the month and how many days are in that month
-     */
-    switch (month)
+    else
     {
-        case 1 :cout<<"31 days "<<endl;break;
-        case 2 :cout<<"28 days "<<endl;break;
-        case 3 :cout<<"31 days "<<endl;break;
-        case 4 :cout<<"30 days "<<endl;break;
-        case 5 :cout<<"31 days "<<endl;break;
-        case 6 :cout<<"30 days "<<endl;break;
-        case 7 :cout<<"31 days "<<endl;break;
-        case 8 :cout<<"31 days "<<endl;break;
-        case 9 :cout<<"30 days "<<endl;break;
-        case 10 :cout<<"31 days "<<endl;break;
-        case 11 :cout<<"30 days "<<endl;break;
-        case 12 :cout<<"31 days "<<endl;break;
-        
-    }
+        if (year%4==0)
+        {
+        cout<<"Leap year"<<endl;
+        switch (month)
+            {
+               case 1 :cout<<"31 days "<<endl;break;
+               case 2 :cout<<"29 days "<<endl;break;
+               case 3 :cout<<"31 days "<<endl;break;
+               case 4 :cout<<"30 days "<<endl;break;
+               case 5 :cout<<"31 days "<<endl;break;
+               case 6 :cout<<"30 days "<<endl;break;
+               case 7 :cout<<"31 days "<<endl;break;
+               case 8 :cout<<"31 days "<<endl;break;
+               case 9 :cout<<"30 days "<<endl;break;
+               case 10 :cout<<"31 days "<<endl;break;
+               case 11 :cout<<"30 days "<<endl;break;
+               case 12 :cout<<"31 days "<<endl;break;
+               default:cout<<"End of Program"<<endl;break;
+           }
+        }
+        else
+        {
+        cout<<"Not a Leap year"<<endl;
+        switch (month)
+           {
+               case 1 :cout<<"31 days "<<endl;break;
+               case 2 :cout<<"28 days "<<endl;break;
+               case 3 :cout<<"31 days "<<endl;break;
+               case 4 :cout<<"30 days "<<endl;break;
+               case 5 :cout<<"31 days "<<endl;break;
+               case 6 :cout<<"30 days "<<endl;break;
+               case 7 :cout<<"31 days "<<endl;break;
+               case 8 :cout<<"31 days "<<endl;break;
+               case 9 :cout<<"30 days "<<endl;break;
+               case 10 :cout<<"31 days "<<endl;break;
+               case 11 :cout<<"30 days "<<endl;break;
+               case 12 :cout<<"31 days "<<endl;break;
+               default:cout<<"End of Program"<<endl;break;
+
+           }
    
-    
+        }
+    }
+  
+      
     //Output Located Here
     cout<<"Month = "<<month<<endl;
     cout<<"Year  = "<<year<<endl;
